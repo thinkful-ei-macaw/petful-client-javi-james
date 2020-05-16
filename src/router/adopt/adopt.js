47b7447a08d894bcdfc5e7f10300791125b40460
James Souksavanh Moua <Smoua1558@gmail.com>
2020-05-16 14:38:35 -0400

    make a 5 second interval on adopt.js

--- src/router/adopt/adopt.js
+++ src/router/adopt/adopt.js
@@ -26,30 +26,48 @@ class Adopt extends React.Component {
     )
   }
 
-//   componentDidMount() {
-//     Promise.all([
-//       fetch(`${config.API_ENDPOINT}/people`),
-//       fetch(`${config.API_ENDPOINT}/pets`)
-//     ])
-//     .then(([people, pets]) => {
-//       if(!people.ok)
-//       return people.json().then((e) => promise.reject(e));
-//       if(!pets.ok)
-//       return pets.json().then((e) => promise.reject(e));
+  componentDidMount = () => {
+    // ideally, we move the fetch requests to another method and call it here
+    Promise.all([
+      fetch(`${config.API_ENDPOINT}/people`),
+      fetch(`${config.API_ENDPOINT}/pets`)
+    ])
+    .then(([people, pets]) => {
+      if(!people.ok)
+      return people.json().then((e) => promise.reject(e));
+      if(!pets.ok)
+      return pets.json().then((e) => promise.reject(e));
 
-//       return promise.all(people.json(), pets.json())
-//       .then(([people, pets]) => {
-//         this.setState({people, pets})
-//       })
-//       .catch((error =>{
-//         console.error({error})
-//       })
-//       )})
+      return promise.all(people.json(), pets.json())
+      .then(([people, pets]) => {
+        this.setState({people, pets})
+        this.updatePeople(); //set an interval
+      })
+      .catch((error =>{
+        console.error({error})
+      })
+      )})
+  }
+
+  updatePeople = () => {
+    // set an interval to every 5 seconds we modify the people array 
+    const modifyPeopleInterval = setInterval(this.modifyPeople, 5000)
+
+    this.setState({ people: modifyPeopleInterval })
+  }
+
+  modifyPeople = () => {
+    // remove the first person and add them to the end of the array and update  state 
+    const { people } = this.state;
+    const firstPerson = people.shift();
+    people.push(firstPerson);
+    return people;
+  }
     
-//   }
-
-
-    
+  componentWillUnMount() {
+    // clear the interval before the component is unmouned from the DOM 
+    clearInterval(this.state.people);
+  }
     
     
     render() {
